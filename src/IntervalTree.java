f09c63b817b97ec80b0da2fc4a7ed13fc71cb191
Ali <alihasanli4life@gmail.com>
2026-02-10 23:36:31 +0330

    feat: add IntervalTree.java file

---
+++ src/IntervalTree.java
@@ -0,0 +1,70 @@
+public class IntervalTree {
+
+    private IntervalNode root;
+
+    // Insert
+    public boolean insert(int start, int end, int taskId) {
+        if (hasConflict(start, end)) {
+            return false; // conflict exists
+        }
+        root = insert(root, start, end, taskId);
+        return true;
+    }
+
+    private IntervalNode insert(IntervalNode node, int start, int end, int taskId) {
+        if (node == null)
+            return new IntervalNode(start, end, taskId);
+
+        if (start < node.start)
+            node.left = insert(node.left, start, end, taskId);
+        else
+            node.right = insert(node.right, start, end, taskId);
+
+        node.maxEnd = Math.max(node.maxEnd, end);
+        return node;
+    }
+
+    // Conflict
+    public boolean hasConflict(int start, int end) {
+        return conflictSearch(root, start, end) != null;
+    }
+
+    public IntervalNode conflictSearch(int start, int end) {
+        return conflictSearch(root, start, end);
+    }
+
+    private IntervalNode conflictSearch(IntervalNode node, int start, int end) {
+        if (node == null)
+            return null;
+
+        if (overlap(node, start, end))
+            return node;
+
+        if (node.left != null && node.left.maxEnd >= start)
+            return conflictSearch(node.left, start, end);
+
+        return conflictSearch(node.right, start, end);
+    }
+
+    private boolean overlap(IntervalNode node, int start, int end) {
+        return start <= node.end && node.start <= end;
+    }
+
+    // Print
+    public void print() {
+        print(root, 0);
+    }
+
+    private void print(IntervalNode node, int level) {
+        if (node == null) return;
+
+        print(node.left, level + 1);
+        System.out.println(
+            "Level " + level +
+            " [Task " + node.taskId +
+            " (" + node.start + "," + node.end +
+            "), max=" + node.maxEnd + "]"
+        );
+        print(node.right, level + 1);
+    }
+}
