8a5676776b9919f52d04b249d741b7eedbe7d1db
Ali <alihasanli4life@gmail.com>
2026-02-12 02:23:02 +0330

    refactor: define IntervalNodes with red black tree

--- src/IntervalNode.java
+++ src/IntervalNode.java
@@ -1,16 +1,30 @@
+enum Color {
+    RED, BLACK
+}
+
 public class IntervalNode {
     int start;
     int end;
     int maxEnd;
     int taskId;
 
+    Color color;
+
     IntervalNode left;
     IntervalNode right;
+    IntervalNode parent;
 
     public IntervalNode(int start, int end, int taskId) {
         this.start = start;
         this.end = end;
         this.taskId = taskId;
         this.maxEnd = end;
+
+        this.color = Color.RED;
+    }
+
+    @Override
+    public String toString() {
+        return "[Task " + taskId + " (" + start + "," + end + ")]";
     }
 }
