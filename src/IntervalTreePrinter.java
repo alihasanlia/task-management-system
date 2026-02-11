4fe1d37875bc8ed84f35960bd22b74021c91bd55
Ali <alihasanli4life@gmail.com>
2026-02-12 03:12:44 +0330

    refactor: define IntervalTreePrinter with red black tree

--- src/IntervalTreePrinter.java
+++ src/IntervalTreePrinter.java
@@ -1,34 +1,47 @@
 public class IntervalTreePrinter {
 
-    public void print(IntervalNode root) {
-        System.out.println("\n" + "=== Interval Tree ===");
-        if (root == null) {
+    private static final String ANSI_RED   = "\u001B[31m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    public void print(IntervalNode root, IntervalNode nil) {
+        System.out.println("\n" + "=== Interval Tree (RB) ===" + ANSI_RESET);
+        if (root == nil) {
             System.out.println("(Empty Tree)");
         } else {
-            printHelper(root.right, "    ", false);
-            printNode(root);
-            printHelper(root.left, "    ", true);
+            printTreeHelper(root.right, nil, "    ", false);
+            
+            // Print root with special formatting
+            System.out.print("─── ");
+            if (root.color == Color.RED) {
+                System.out.println(ANSI_RED + root.toString() + ANSI_RESET);
+            } else {
+                System.out.println(ANSI_BLACK + root.toString() + ANSI_RESET);
+            }
+            
+            printTreeHelper(root.left, nil, "    ", true);
         }
-        System.out.println("=====================\n");
+        System.out.println("==========================\n");
     }
 
-    private void printHelper(IntervalNode node, String indent, boolean isLeft) {
-        if (node != null) {
+    private void printTreeHelper(IntervalNode node, IntervalNode nil, String indent, boolean isLeft) {
+        if (node != nil) {
             // Print right subtree first
-            printHelper(node.right, indent + (isLeft ? "│   " : "    "), false);
+            printTreeHelper(node.right, nil, indent + (isLeft ? "│   " : "    "), false);
             
             // Print current node
             System.out.print(indent);
             System.out.print(isLeft ? "└── " : "┌── ");
             
-            printNode(node);
+            if (node.color == Color.RED) {
+                System.out.println(ANSI_RED + node.toString() + ANSI_RESET);
+            } else {
+                System.out.println(ANSI_BLACK + node.toString() + ANSI_RESET);
+            }
             
             // Print left subtree
-            printHelper(node.left, indent + (isLeft ? "    " : "│   "), true);
+            printTreeHelper(node.left, nil, indent + (isLeft ? "    " : "│   "), true);
         }
     }
-
-    private void printNode(IntervalNode node) {
-        System.out.println("Task " + node.taskId +" (" + node.start + "," + node.end + ")");
-    }
+    
 }
