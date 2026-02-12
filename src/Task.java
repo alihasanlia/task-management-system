f89ccef99c111b073be1c587d4a0a2f271d4f539
Ali <alihasanli4life@gmail.com>
2026-02-12 19:50:29 +0330

    refactor: improve task visualiztion

--- src/Task.java
+++ src/Task.java
@@ -11,11 +11,24 @@ public class Task {
         this.value = value;
     }
 
-    @Override
-    public String toString() {
-        return "Task{id=" + id +
-               ", start=" + startTime +
-               ", end=" + endTime +
-               ", value=" + value + "}";
-    }
+    // Color constants
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_ORANGE = "\u001B[38;5;208m";
+    private static final String ANSI_BOLD = "\u001B[1m";
+
+    public void print() {
+    String interval = String.format("[%2d - %2d]", startTime, endTime);
+    String bar = "|".repeat(endTime - startTime + 1);
+
+    System.out.println(
+            ANSI_BOLD + "Task" + String.format("%2d", id) + ": " +  ANSI_RESET +
+            ANSI_ORANGE + interval + " " + ANSI_RESET +
+            ANSI_BOLD + "Value: " + ANSI_RESET +
+            ANSI_ORANGE + String.format("%3d", value) + ANSI_RESET + " " +
+            ANSI_GREEN + bar + ANSI_RESET
+    );
+}
+
+
 }
