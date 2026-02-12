f88e6ad77d26a9c36cd0fb5dd5626699515d1e17
Ali <alihasanli4life@gmail.com>
2026-02-12 14:46:45 +0330

    feat: implement IntervalTree deletion

--- src/IntervalTree.java
+++ src/IntervalTree.java
@@ -90,6 +90,140 @@ public class IntervalTree {
         root.color = Color.BLACK;
     }
 
+    // Delete
+    public void delete(int start, int end, int taskId) {
+        IntervalNode z = findNode(root, start, end, taskId);
+        if (z == NIL) return;
+
+        bstDelete(z);
+    }
+
+    private IntervalNode findNode(IntervalNode x, int start, int end, int taskId) {
+        while (x != NIL) {
+            if (x.start == start && x.end == end && x.taskId == taskId)
+                return x;
+            else if (start < x.start)
+                x = x.left;
+            else
+                x = x.right;
+        }
+        return NIL;
+    }
+
+    private void bstDelete(IntervalNode z) {
+        IntervalNode y = z;
+        Color yOriginalColor = y.color;
+        IntervalNode x;
+
+        if (z.left == NIL) {
+            x = z.right;
+            transplant(z, z.right);
+        } else if (z.right == NIL) {
+            x = z.left;
+            transplant(z, z.left);
+        } else {
+            y = minimum(z.right);
+            yOriginalColor = y.color;
+            x = y.right;
+
+            if (y.parent == z)
+                x.parent = y;
+            else {
+                transplant(y, y.right);
+                y.right = z.right;
+                y.right.parent = y;
+            }
+
+            transplant(z, y);
+            y.left = z.left;
+            y.left.parent = y;
+            y.color = z.color;
+
+            updateMaxUpwards(y);
+        }
+
+        if (yOriginalColor == Color.BLACK)
+            fixDelete(x);
+    }
+
+    private void transplant(IntervalNode u, IntervalNode v) {
+        if (u.parent == NIL)
+            root = v;
+        else if (u == u.parent.left)
+            u.parent.left = v;
+        else
+            u.parent.right = v;
+
+        v.parent = u.parent;
+    }
+
+    private void fixDelete(IntervalNode x) {
+        while (x != root && x.color == Color.BLACK) {
+            if (x == x.parent.left) {
+                IntervalNode w = x.parent.right;
+
+                if (w.color == Color.RED) {
+                    w.color = Color.BLACK;
+                    x.parent.color = Color.RED;
+                    leftRotate(x.parent);
+                    w = x.parent.right;
+                }
+
+                if (w.left.color == Color.BLACK && w.right.color == Color.BLACK) {
+                    w.color = Color.RED;
+                    x = x.parent;
+                } else {
+                    if (w.right.color == Color.BLACK) {
+                        w.left.color = Color.BLACK;
+                        w.color = Color.RED;
+                        rightRotate(w);
+                        w = x.parent.right;
+                    }
+                    w.color = x.parent.color;
+                    x.parent.color = Color.BLACK;
+                    w.right.color = Color.BLACK;
+                    leftRotate(x.parent);
+                    x = root;
+                }
+            } else {
+                IntervalNode w = x.parent.left;
+
+                if (w.color == Color.RED) {
+                    w.color = Color.BLACK;
+                    x.parent.color = Color.RED;
+                    rightRotate(x.parent);
+                    w = x.parent.left;
+                }
+
+                if (w.right.color == Color.BLACK && w.left.color == Color.BLACK) {
+                    w.color = Color.RED;
+                    x = x.parent;
+                } else {
+                    if (w.left.color == Color.BLACK) {
+                        w.right.color = Color.BLACK;
+                        w.color = Color.RED;
+                        leftRotate(w);
+                        w = x.parent.left;
+                    }
+                    w.color = x.parent.color;
+                    x.parent.color = Color.BLACK;
+                    w.left.color = Color.BLACK;
+                    rightRotate(x.parent);
+                    x = root;
+                }
+            }
+        }
+        x.color = Color.BLACK;
+
+        updateMaxUpwards(x);
+    }
+
+    private IntervalNode minimum(IntervalNode x) {
+        while (x.left != NIL)
+            x = x.left;
+        return x;
+    }
+
     // Rotations
     private void leftRotate(IntervalNode x) {
         IntervalNode y = x.right;
